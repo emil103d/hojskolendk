d00b372d9ec968c8f14aebf53c11da71c169f994
Soph0298 <Soph0298@stud.kea.dk>
2022-05-25 18:37:53 +0200

    myfooter test

---
+++ src/routes/MyFooter.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<section>
+  <div>
+    <div class="">
+      <div><img src="" alt="" /></div>
+      <h2>Ring til os</h2>
+      <p>dingeling</p>
+    </div>
+  </div>
+</section>
+
+<style>
+</style>
