eded1e4129659cb370c96e8c9ec2c53235ace28d
emilie carlsen <emil103d@stud.kea.dk>
2022-05-26 21:35:38 +0200

    Så virker side navigationen!!!!

---
+++ src/routes/singlepage.svelte
@@ -0,0 +1,26 @@
+<script>
+  import TabA from "../components/tabA.svelte";
+  import TabB from "../components/tabB.svelte";
+  import TabC from "../components/tabC.svelte";
+
+  let activetab = "TabA";
+</script>
+
+<main>
+  <button on:click={() => (activetab = "TabA")}>Tab A</button>
+  <button on:click={() => (activetab = "TabB")}>Tab B</button>
+  <button on:click={() => (activetab = "TabC")}>Tab C</button>
+
+  {#if activetab === "TabA"}
+    <TabA />
+  {/if}
+  {#if activetab === "TabB"}
+    <TabB />
+  {/if}
+  {#if activetab === "TabC"}
+    <TabC />
+  {/if}
+</main>
+
+<style>
+</style>
