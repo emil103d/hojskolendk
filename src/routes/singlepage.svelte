4af12c8afcfb508863bae849c5049d3b95a6e226
emilie carlsen <emil103d@stud.kea.dk>
2022-05-26 22:15:20 +0200

    Alle single sider er oprettet

--- src/routes/singlepage.svelte
+++ src/routes/singlepage.svelte
@@ -1,26 +1,61 @@
 <script>
-  import TabA from "../components/tabA.svelte";
-  import TabB from "../components/tabB.svelte";
-  import TabC from "../components/tabC.svelte";
+  import Tab1 from "../components/tab1.svelte";
+  import Tab2 from "../components/tab2.svelte";
+  import Tab3 from "../components/tab3.svelte";
+  import Tab4 from "../components/tab4.svelte";
+  import Tab5 from "../components/tab5.svelte";
+  import Tab6 from "../components/tab6.svelte";
+  import Tab7 from "../components/tab7.svelte";
+  import Tab8 from "../components/tab8.svelte";
+  import Tab9 from "../components/tab9.svelte";
+  import Tab10 from "../components/tab10.svelte";
 
-  let activetab = "TabA";
+  let activetab = Tab1;
 </script>
 
 <main>
-  <button on:click={() => (activetab = "TabA")}>Tab A</button>
-  <button on:click={() => (activetab = "TabB")}>Tab B</button>
-  <button on:click={() => (activetab = "TabC")}>Tab C</button>
+  <section class="grid grid-cols-2">
+    <sidebar class="block">
+      <h2 class="text:sm m:1 mb:6">Alt du skal vide om opholdet</h2>
+      <button class="" on:click={() => (activetab = Tab1)}
+        >OPHOLDSOVERSIGT</button
+      >
+      <button on:click={() => (activetab = Tab2)}>HVAD ER INKLUDERET</button>
+      <button on:click={() => (activetab = Tab3)}>PROGRAM</button>
+      <button on:click={() => (activetab = Tab4)}>UNDERVISNINGEN</button>
+      <button on:click={() => (activetab = Tab5)}>AFREJSEDATOER</button>
+      <button on:click={() => (activetab = Tab6)}>DOGT AT VIDE</button>
+      <button on:click={() => (activetab = Tab7)}>FRIUGER</button>
 
-  {#if activetab === "TabA"}
-    <TabA />
-  {/if}
-  {#if activetab === "TabB"}
-    <TabB />
-  {/if}
-  {#if activetab === "TabC"}
-    <TabC />
-  {/if}
+      <button on:click={() => (activetab = Tab8)}>ADVENTURE</button>
+      <button on:click={() => (activetab = Tab9)}>REJSEBREVE</button>
+      <button on:click={() => (activetab = Tab10)}>DIN REJSELEDER</button>
+    </sidebar>
+    <svelte:component this={activetab} />
+    <!-- {#if activetab === "TabA"}
+        <TabA />
+      {/if}
+      {#if activetab === "TabB"}
+        <TabB />
+      {/if}
+      {#if activetab === "TabC"}
+        <TabC />
+      {/if} -->
+  </section>
 </main>
 
 <style>
+  button {
+    border-bottom: 2px solid #545454;
+    min-width: 12rem;
+    text-align: left;
+    margin: 1rem;
+    display: block;
+  }
+
+  button:focus,
+  button:hover {
+    border-bottom: 2px solid #545454;
+    font-weight: 800;
+  }
 </style>
