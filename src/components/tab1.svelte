5b780bcbd7c75f80bc28e087851d4474d5df0b03
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-05-27 15:09:23 +0200

    tab1 done mobil

--- src/components/tab1.svelte
+++ src/components/tab1.svelte
@@ -12,32 +12,91 @@
   <!-- SECTION 2 -->
 <section>
   <div class="container grid grid-cols-2 grid-rows-2 gap-2 text-center">
-    <div class="col-start-1 row-start-1 bokse">
-      <div class="">
+    <div class="col-start-1 row-start-1 infobokse">
+        <img class="h-8 w-auto m-auto" src="src/_icons/destination.png" alt="">
         <h3>Destinationer</h3>
         <p>Sri Lanka, Maldiverne & Thailand</p>
-      </div>
     </div>
-    <div class="col-start-2 row-start-1 bokse"> 
+    <div class="col-start-2 row-start-1 infobokse"> 
+      <img class="h-8 w-auto m-auto" src="src/_icons/calender.png" alt="">
       <h3>Varighed</h3>
       <p>3 måneder</p>
     </div>
-    <div class="col-start-1 row-start-2 bokse">
+    <div class="col-start-1 row-start-2 infobokse">
+      <img class="h-8 w-auto m-auto" src="src/_icons/faelleskab.png" alt="">
       <h3>Undervisning </h3> 
       <p>Selvudvikling & Kommunikation</p> 
     </div>
-    <div class="col-start-2 row-start-2 bokse"> 
+    <div class="col-start-2 row-start-2 infobokse "> 
+      <img class="h-8 w-auto m-auto" src="src/_icons/price.png" alt="">
       <h3>Pris </h3> 
       <p>59.700 DKK</p> 
     </div>
   </div>
 </section>
 
+<section class="baggrund-klode relative py-4">
+  <p>Hvad vi har planlagt</p>
+  <h2>Overblik</h2>
+
+  <div class="overflow-scroll max-h-[30vh] relative bg-gray-300 mx-3 p-4">
+    <p> <b>Uge 1</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+
+    <p> <b>Uge 2</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+
+    <p> <b>Uge 3</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+
+    <p> <b>Uge 4</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+
+    <p> <b>Uge 5</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+
+    <p> <b>Uge 6</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+  
+    <p> <b>Uge 7</b> </p>
+    <p>Rejsen starter i København lufthavn.</p>
+  </div>
+</section>
+
+<section>
+  <p>Hvad vi har planlagt</p>
+  <h2>Overblik</h2>
+  <div class="grid grid-cols-3 grid-rows-3">
+    <img class="col-start-1 row-start-1 max-h-96" src="src/_images/1.jpg" alt="">
+    <img class="col-start-2 row-start-1 max-h-96" src="src/_images/1.jpg" alt="">
+    <img class="col-start-3 row-start-1 max-h-96"src="src/_images/1.jpg" alt="">
+    <img class="col-start-1 row-start-2 max-h-96" src="src/_images/1.jpg" alt="">
+    <img class="col-start-2 row-start-2 max-h-96" src="src/_images/1.jpg" alt="">
+    <img class="col-start-3 row-start-2 max-h-96"src="src/_images/1.jpg" alt="">
+    <img class="col-start-1 row-start-3 max-h-96" src="src/_images/1.jpg" alt="">
+    <img class="col-start-2 row-start-3 max-h-96" src="src/_images/1.jpg" alt="">
+    <img class="col-start-3 row-start-3 max-h-96"src="src/_images/1.jpg" alt="">
+  </div>
+</section>
+
+
+
 <style>
-  .bokse {
+  .infobokse {
     background-color: var(--sandbeach);
+    display: grid;
+    place-content: center;
+    gap: 0.3rem;
+    padding: 2rem;
   }
+
+ .baggrund-klode {
+  background-color: red;
+ }
+
+
 </style>
+
 
 
 
