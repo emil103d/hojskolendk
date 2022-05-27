4b2dc2cec556050f33a0c07385976e85fe4867e6
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-05-27 11:56:00 +0200

    ANJ test tab-1

--- src/components/tab1.svelte
+++ src/components/tab1.svelte
@@ -1,9 +1,22 @@
-<section>
-  <h2>SURFING, SOL OG BEACH VIBES</h2>
-  <p>
+
+
+
+
+<!-- Section 1 -->
+<div class="max">
+
+
+
+    <h2>SURFING, SOL OG BEACH VIBES</h2>
+    <p>
     Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på Rumrunners
     eller snorkler efter havskildpadder. Derudover har vi haft en masse fede
     ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck Island og
     turtlewatch på programmet.
-  </p>
-</section>
+    </p>
+</div>
+
+  x
+
+
+
