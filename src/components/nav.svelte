771c2e9cefd3daa78fd97ff7e2110bc0f8908f17
emilie carlsen <emil103d@stud.kea.dk>
2022-05-28 17:49:38 +0200

    Grov navbar

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -17,15 +17,15 @@
 
   // Attach media query listener on mount hook
   onMount(() => {
-    const mediaListener = window.matchMedia("(max-width: 767px)");
+    const mediaListener = window.matchMedia("(max-width: 1000px)");
 
     mediaListener.addListener(mediaQueryHandler);
   });
 </script>
 
-<nav class="w-screen grid grid-cols-10 md:grid-cols-3">
-  <img class="cols-span-4" src="../src/_logo/logohvid.png" alt="logo hvid" />
-  <div class="inner cols-span-6 justify-center ">
+<nav class="w-screen flex md:grid md:grid-cols-3">
+  <img class="" src="../src/_logo/logohvid.png" alt="logo hvid" />
+  <div class="inner  justify-end ">
     <div
       on:click={handleMobileIconClick}
       class={`mobile-icon${showMobileMenu ? " active" : ""}`}
@@ -33,7 +33,7 @@
       <div class="middle-line" />
     </div>
     <ul class={`navbar-list${showMobileMenu ? " mobile" : ""}`}>
-      <li id="" class="bg-black">
+      <li id="" class="bg-black md:hidden">
         <div
           class="accordion justify-center flex-col flex overflow-y-scroll pb-10 flex-nowrap"
         >
@@ -166,13 +166,13 @@
         </div>
       </li>
 
-      <li class="hidden md:block">
+      <li class="hidden lg:block">
         <div class="group relative h-full">
           <a
-            href="#"
+            href="/alleophold"
             class="text-white flex items-center h-full bg-grey-darkest px-4"
           >
-            Account
+            Højskoleophold
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -186,79 +186,145 @@
             >
           </a>
           <div
-            class="hidden group-hover:block absolute pin-r top-full w-48 bg-black"
+            id="hover_underkategori"
+            class="hidden p-4 pb-6 group-hover:grid absolute pin-r top-full grid-cols-3 grid-rows-2 bg-black -left-2/3"
           >
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              My Account
-            </a>
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              Edit Account
-            </a>
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              Logout
-            </a>
+            <div id="hover_underkategori" class="col-span-3 flex items-center">
+              <img class="ikoner" src="../src/_icons/flyafgang.png" alt="" />
+
+              <a
+                href="/alleophold"
+                class="block text-left py-3 px-3 text-white text-lg "
+              >
+                Se alle ophold
+              </a>
+            </div>
+
+            <div class="columns-1 block">
+              <div class="flex items-center">
+                <img class="ikoner" src="../src/_icons/flyafgang.png" alt="" />
+
+                <h4 class="text-white">5 uger</h4>
+              </div>
+              <ul class="block">
+                <li class="text-white flex items-center">
+                  <svg
+                    class=" ml-4"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="5" cy="5" r="5" fill="white" />
+                  </svg>
+                  <a href="/singlepage">BALI, LOMBOK & GILI ISLANDS</a>
+                </li>
+                <li class="text-white flex items-center">
+                  <svg
+                    class=" ml-4"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="5" cy="5" r="5" fill="white" />
+                  </svg>
+                  <a href="/singlepage">BALI, LOMBOK & GILI ISLANDS</a>
+                </li>
+              </ul>
+            </div>
+            <div class="columns-1 block justify-start">
+              <div class="flex items-center">
+                <img class="ikoner" src="../src/_icons/flyafgang.png" alt="" />
+
+                <h4 class="text-white">2 måneder ophold</h4>
+              </div>
+              <ul class="block">
+                <li class="text-white flex items-center">
+                  <svg
+                    class=" ml-4"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="5" cy="5" r="5" fill="white" />
+                  </svg>
+                  <a href="/singlepage">BALI, LOMBOK & GILI ISLANDS</a>
+                </li>
+                <li class="text-white flex items-center">
+                  <svg
+                    class=" ml-4"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="5" cy="5" r="5" fill="white" />
+                  </svg>
+                  <a href="/singlepage">BALI, LOMBOK & GILI ISLANDS</a>
+                </li>
+              </ul>
+            </div>
+            <div class="columns-1 block">
+              <div class="flex items-center">
+                <img class="ikoner" src="../src/_icons/flyafgang.png" alt="" />
+
+                <h4 class="text-white">3 måneders ophold</h4>
+              </div>
+              <ul class="block">
+                <li class="text-white flex items-center">
+                  <svg
+                    class=" ml-4"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="5" cy="5" r="5" fill="white" />
+                  </svg>
+                  <a href="/singlepage">BALI, LOMBOK & GILI ISLANDS</a>
+                </li>
+                <li class="text-white flex items-center">
+                  <svg
+                    class=" ml-4"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="5" cy="5" r="5" fill="white" />
+                  </svg>
+                  <a href="/singlepage">BALI, LOMBOK & GILI ISLANDS</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </li>
       <li class="hidden md:block">
         <div class="group relative h-full">
           <a
-            href="#"
+            href="/infomode"
             class="text-white flex items-center h-full bg-grey-darkest px-4"
           >
-            Account
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              class="h-6 w-6 text-grey-darker fill-current ml-1"
-              ><path
-                class="heroicon-ui"
-                d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
-              /></svg
-            >
+            Infomøder
           </a>
-          <div
-            class="hidden group-hover:block absolute pin-r top-full w-48 bg-black"
-          >
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              My Account
-            </a>
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              Edit Account
-            </a>
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              Logout
-            </a>
-          </div>
         </div>
       </li>
       <li class="hidden md:block">
         <div class="group relative h-full">
           <a
-            href="#"
+            href="/#"
             class="text-white flex items-center h-full bg-grey-darkest px-4"
           >
-            Account
+            Om os
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -274,24 +340,110 @@
           <div
             class="hidden group-hover:block absolute pin-r top-full w-48 bg-black"
           >
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              My Account
-            </a>
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              Edit Account
-            </a>
-            <a
-              href="#"
-              class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
-            >
-              Logout
-            </a>
+            <ul>
+              <li>
+                <a
+                  href="/#"
+                  class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
+                >
+                  Møde teamet
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#"
+                  class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
+                >
+                  Vores værdier
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#"
+                  class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
+                >
+                  Bæredygtighed
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#"
+                  class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
+                >
+                  FAQ
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#"
+                  class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
+                >
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#"
+                  class="block text-left py-3 px-3 text-white hover:text-blue-dark text-xs"
+                >
+                  Det siger vores elever
+                </a>
+              </li>
+            </ul>
+            <div class="footer_some mr-3 mb-6">
+              <div class="h-1 w-full bg-slate-500 pr-12" />
+              <p class="text-center">#hojskolen dk</p>
+              <div>
+                <svg
+                  width="202"
+                  height="30"
+                  viewBox="0 0 202 47"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="71.5"
+                    y="1.01514"
+                    width="59"
+                    height="44.6425"
+                    rx="10.5"
+                    stroke="white"
+                  />
+                  <path
+                    d="M95 35.0005V11.6721L112 23.3363L95 35.0005Z"
+                    stroke="white"
+                  />
+                  <path
+                    d="M1 19.2793H11C12.0003 16.2364 15.9009 10.1508 23.5009 10.1508C31.1009 10.1508 34.6676 16.2364 35.5009 19.2793H45.0009C45.0009 16.0674 45.0018 9.23792 45.0009 5.58652C45 2.03655 40.619 0.515137 38 0.515137H6.5C3.5 0.515137 0.999095 3.05083 1 5.07938V19.2793Z"
+                    stroke="white"
+                  />
+                  <path
+                    d="M1 23.3363H10.1466C10.1466 31.4505 15.9002 36.5219 23.5 36.5219C31.0998 36.5219 36 29.422 36 23.3363H44.9996C44.9996 27.1558 45.0005 35.2774 44.9996 39.6196C44.9987 43.8411 40.6178 45.6504 37.9989 45.6504H6.49984C3.49993 45.6504 0.999095 42.635 1 40.2226V23.3363Z"
+                    stroke="white"
+                  />
+                  <path
+                    d="M32.5 23.3364C32.5 28.6686 28.24 32.9791 23 32.9791C17.76 32.9791 13.5 28.6686 13.5 23.3364C13.5 18.0041 17.76 13.6936 23 13.6936C28.24 13.6936 32.5 18.0041 32.5 23.3364Z"
+                    stroke="white"
+                  />
+                  <rect
+                    x="34.5"
+                    y="4.56519"
+                    width="7"
+                    height="7.11421"
+                    rx="0.5"
+                    stroke="white"
+                  />
+                  <path
+                    d="M200.543 23.3364C200.543 35.681 190.89 45.6576 179.021 45.6576C167.152 45.6576 157.5 35.681 157.5 23.3364C157.5 10.9917 167.152 1.01514 179.021 1.01514C190.89 1.01514 200.543 10.9917 200.543 23.3364Z"
+                    stroke="white"
+                  />
+                  <path
+                    d="M182.372 45.1865V29.6486H187.16L188.596 22.8508H182.372C182.372 21.3941 182.372 18.3837 182.372 17.9952C182.372 17.5097 182.851 16.053 184.287 15.5674C185.436 15.179 187.638 15.4056 188.596 15.5674V9.74072C186.521 9.74072 182.181 9.74072 181.415 9.74072C179.979 9.74072 178.356 10.4785 177.106 12.1685C175.67 14.1107 175.67 16.053 175.67 17.0241V22.8508H170.404V29.6486H175.67V45.1865"
+                    stroke="white"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
       </li>
@@ -301,7 +453,7 @@
             href="/#"
             class="text-white flex items-center h-full bg-grey-darkest px-4"
           >
-            Account
+            Elevforening
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -367,10 +519,14 @@
     z-index: 30;
     align-self: center;
   }
+
+  .ikoner {
+    width: 2rem;
+    margin: 1rem;
+  }
   .inner {
     max-width: 980px;
     padding-left: 20px;
-    padding-right: 10%;
     margin: auto;
     box-sizing: border-box;
     display: flex;
@@ -506,7 +662,11 @@
     max-height: 50em;
   }
 
-  @media only screen and (min-width: 767px) {
+  #hover_underkategori {
+    width: 56rem;
+  }
+
+  @media only screen and (min-width: 1000px) {
     .mobile-icon {
       display: none;
     }
