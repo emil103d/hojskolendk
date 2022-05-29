f1640ea34f5c41cf9935ca365287c3f48c0acc75
Soph0298 <Soph0298@stud.kea.dk>
2022-05-29 14:21:42 +0200

    header

--- src/components/tab4.svelte
+++ src/components/tab4.svelte
@@ -1,1 +1,19 @@
-<h2>Undervisningen</h2>
+<script>
+  import Header from "./header.svelte";
+</script>
+
+<section class="col-span-2">
+  <Header imgtitle="src/_images/1.jpg" />
+</section>
+
+<main class="col-start-2">
+  <section>
+    <h2>UNDERVISNING</h2>
+    <p>
+      Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på
+      Rumrunners eller snorkler efter havskildpadder. Derudover har vi haft en
+      masse fede ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck
+      Island og turtlewatch på programmet.
+    </p>
+  </section>
+</main>
