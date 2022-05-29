040b876c5db84ff6c10c98a3d72e9527a5d0527b
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-05-29 20:41:38 +0200

    Tab3 fucker med mig.

--- src/components/tab3.svelte
+++ src/components/tab3.svelte
@@ -1,129 +1,124 @@
-  <main class="col-start-2">
-   <!-- SECTION 1 -->
-   <section>
-    <h2>SURFING, SOL OG BEACH VIBES</h2>
-    <p>
-    Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på Rumrunners
-    eller snorkler efter havskildpadder. Derudover har vi haft en masse fede
-    ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck Island og
-    turtlewatch på programmet.
-    </p>
-
-    <img class="w-full hidden lg:block" src="src/_images/programoversigt.png" alt="">
-</section>
-
- <!-- SECTION 2 -->
- <section class="lg:grid md:grid-cols-2">
-  <div class="lg:col-start-1">
-      <p>UGE 1:</p>
-      <h4>Introuge</h4>
-      <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
-          Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
-      </p>
-  </div>
-  <div class="lg:col-start-2">
-      <img class="w-full h-auto"src="src/_images/program_1.png" alt="">
-  </div>
-</section>
-
-  <!-- SECTION 3 -->
-  <section class="">
-    <div>
-        <p>UGE 2 + 3 + 4:</p>
-        <h4>projektstyring & Teambuilding</h4>
-        <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
-            Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
-        </p>
-    </div>
-    <div>
-        <div class="flex overflow-x-scroll pb-10 flex-nowrap overflow-hidden overflow-x-hidden">
-            <div class="flex flex-nowrap gap-2 lg:grid lg:grid-cols-3">
-                <img class="max-w-[90vw] h-full md:w-full md:col-start-1" src="src/_images/program_2.png" alt="">
-                <img class="max-w-[90vw] h-full md:w-full md:col-start-2" src="src/_images/program_3.png" alt="">
-                <img class="max-w-[90vw] h-full md:w-full md:col-start-3" src="src/_images/program_4.png" alt="">
-            </div>
-          </div>
-    </div>
-  </section>
-
-
-   <!-- SECTION 4 -->
-   <section class="lg:grid md:grid-cols-2 lg:grid-rows-1">
-    <div class="lg:col-start-2 md:row-start-1">
-        <p>UGE 5</p>
-        <h4>FRIUGE - MALDIVERNE</h4>
-        <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
-            Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
-        </p>
-    </div>
-    <div class="lg:col-start-1">
-        <img src="src/_images/program_1.png" alt="">
-    </div>
-  </section>
-
-  <!-- SECTION 5 -->
-  <section class="">
-    <div>
-        <p>UGE 6 + 7 + 8:</p>
-        <h4>Selvudvikling & Kommunikation</h4>
-        <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
-            Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
-        </p>
-    </div>
-    <div>
-        <div class="flex overflow-x-scroll pb-10 flex-nowrap overflow-hidden overflow-x-hidden">
-            <div class="flex flex-nowrap gap-2">
-                <img class="" src="src/_images/program_5.png" alt="">
-                <img class="" src="src/_images/program_6.png" alt="">
-                <img class="" src="src/_images/program_7.png" alt="">
-            </div>
-          </div>
-    </div>
-  </section>
-
-  <!-- SECTION 6 -->
-  <section class="lg:grid lg:grid-cols-2 lg:grid-rows-1">
-    <div class="lg:col-start-1 lg:row-start-1">
-        <p>UGE 9</p>
-        <h4>FRIUGE - SRI LANKA</h4>
-        <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
-            Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
-        </p>
-    </div>
-    <div class="lg:col-start-2 lg:row-start-1">
-        <img src="src/_images/program_9.png" alt="">
-    </div>
-</section>
-
-
-<!-- SECTION 7 -->
-<section class="lg:grid lg:grid-cols-2 lg:grid-rows-1">
-  <div class="lg:col-start-2 lg:row-start-1">
-      <p>UGE 10</p>
-      <h4>OUTRODAGE</h4>
-      <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
-          Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
-      </p>
-  </div>
-  <div class="lg:col-start-1 lg:row-start-1">
-      <img src="src/_images/program_8.png" alt="">
-  </div>
-</section>
-
-
-
-
-</main>
-
-
-
-<style>
-
-
-
-
-</style>
-
-
-
+<main class="col-start-2">
+    <!-- SECTION 1 -->
+    <section>
+     <h2>SURFING, SOL OG BEACH VIBES</h2>
+     <p>
+     Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på Rumrunners
+     eller snorkler efter havskildpadder. Derudover har vi haft en masse fede
+     ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck Island og
+     turtlewatch på programmet.
+     </p>
+ </section>
+ 
+  <!-- SECTION 2 -->
+  <section class="lg:grid md:grid-cols-2">
+   <div class="lg:col-start-1">
+       <p>Aktiviteter i friugen</p>
+       <h4>Uge 1 - Sri Lanka</h4>
+       <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
+           Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
+       </p>
+   </div>
+   <div class="lg:col-start-2">
+       <img class="w-full h-auto"src="src/_images/program_1.png" alt="">
+   </div>
+ </section>
+ 
+   <!-- SECTION 3 -->
+   <section class="">
+     <div>
+         <p>UGE 2 + 3 + 4:</p>
+         <h4>projektstyring & Teambuilding</h4>
+         <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
+             Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
+         </p>
+     </div>
+     <div>
+         <div class="flex overflow-x-scroll pb-10 flex-nowrap overflow-hidden">
+             <div class="flex flex-nowrap gap-2 lg:grid lg:grid-cols-3">
+                 <img class="max-w-[90vw] h-full md:w-full md:col-start-1" src="src/_images/program_2.png" alt="">
+                 <img class="max-w-[90vw] h-full md:w-full md:col-start-2" src="src/_images/program_3.png" alt="">
+                 <img class="max-w-[90vw] h-full md:w-full md:col-start-3" src="src/_images/program_4.png" alt="">
+             </div>
+           </div>
+     </div>
+   </section>
+ 
+ 
+    <!-- SECTION 4 -->
+    <section class="lg:grid md:grid-cols-2 lg:grid-rows-1">
+     <div class="lg:col-start-2 md:row-start-1">
+         <p>UGE 5</p>
+         <h4>FRIUGE - MALDIVERNE</h4>
+         <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
+             Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
+         </p>
+     </div>
+     <div class="lg:col-start-1">
+         <img src="src/_images/program_1.png" alt="">
+     </div>
+   </section>
+ 
+   <!-- SECTION 5 -->
+   <section class="">
+     <div>
+         <p>UGE 6 + 7 + 8:</p>
+         <h4>Selvudvikling & Kommunikation</h4>
+         <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
+             Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
+         </p>
+     </div>
+     <div>
+         <div class="flex overflow-x-scroll pb-10 flex-nowrap overflow-hidden">
+             <div class="flex flex-nowrap gap-2">
+                 <img class="" src="src/_images/program_5.png" alt="">
+                 <img class="" src="src/_images/program_6.png" alt="">
+                 <img class="" src="src/_images/program_7.png" alt="">
+             </div>
+           </div>
+     </div>
+   </section>
+ 
+   <!-- SECTION 6 -->
+   <section class="lg:grid lg:grid-cols-2 lg:grid-rows-1">
+     <div class="lg:col-start-1 lg:row-start-1">
+         <p>UGE 9</p>
+         <h4>FRIUGE - SRI LANKA</h4>
+         <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
+             Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
+         </p>
+     </div>
+     <div class="lg:col-start-2 lg:row-start-1">
+         <img src="src/_images/program_9.png" alt="">
+     </div>
+ </section>
+ 
+ 
+ <!-- SECTION 7 -->
+ <section class="lg:grid lg:grid-cols-2 lg:grid-rows-1">
+   <div class="lg:col-start-2 lg:row-start-1">
+       <p>UGE 10</p>
+       <h4>OUTRODAGE</h4>
+       <p>Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. Eda bokstav bejåligt. Heminade abel oaktat prest. Nekes kolertad genuskänslig.Lörem ipsum får kende. Tredons bida dir rödgrönrosa. Sona dotivis. 
+           Eda bokstav bejåligt. Heminade abel oaktat prest. bejåligt. Heminade abel oaktat prest. 
+       </p>
+   </div>
+   <div class="lg:col-start-1 lg:row-start-1">
+       <img src="src/_images/program_8.png" alt="">
+   </div>
+ </section>
+ 
+ 
+ 
+ 
+ </main>
+ 
+ 
+ 
+ <style>
+ 
+ 
+ 
+ 
+ </style>
 
