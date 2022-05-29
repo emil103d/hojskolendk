846f0e25404f8224d143b6ab140077b0a7d85dca
Soph0298 <Soph0298@stud.kea.dk>
2022-05-29 10:39:54 +0200

    soph

---
+++ src/components/buycard.svelte
@@ -0,0 +1,24 @@
+<script>
+  import PrimaryButton from "./primarybutton copy.svelte";
+</script>
+
+<section class="flex flex-row">
+  <div><h2>SRI LANKA, MALDIVERNE & THAILAND</h2></div>
+  <div>
+    <div><img src="src/_icons/calender.png" alt="flyafgang ikon" /></div>
+    <div class="grid-col-1">
+      <p><strong>Varighed</strong></p>
+      <p>3 måneder</p>
+    </div>
+    <div class="linediv" />
+  </div>
+  <div><PrimaryButton>TILMELD OPHOLD</PrimaryButton></div>
+</section>
+
+<style>
+  .linediv {
+    height: 10px;
+    width: 1px;
+    background-color: var(--txtcolor);
+  }
+</style>
--- src/components/myfotter.svelte
+++ src/components/myfotter.svelte
@@ -1,9 +1,11 @@
 <script>
   import SecondaryButton from "../components/secondarybutton.svelte";
   import Header from "../components/header.svelte";
+  import BuyCard from "../components/buycard.svelte";
 </script>
 
 <Header headerclass="src/_images/1.jpg" />
+<BuyCard />
 
 <div class="bgfooter">
   <div class="maxwidthwrapper">
