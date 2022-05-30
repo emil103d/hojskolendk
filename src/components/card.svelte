0d8ae3a6be39d140cc59487bd210fe11d639cf9e
Soph0298 <Soph0298@stud.kea.dk>
2022-05-30 16:01:53 +0200

    sop

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -5,7 +5,7 @@
 
 <div class=" mx-auto my-8 relative z-0">
   <img src={image} alt={title} />
-  <h3 class="bg-white pl-3 pt-3 pb-3 pr-0 z-3 absolute top-4 text-xs">
+  <h3 class="bg-white pl-3 pt-3 pb-3 pr-3 z-3 absolute top-4 text-xs">
     {title}
   </h3>
 </div>
@@ -14,10 +14,15 @@
   div {
     min-height: 24rem;
     min-width: 15rem;
+    transition: transform 0.2s;
   }
 
   img {
     height: 24rem;
     width: 15rem;
+  }
+
+  div:hover {
+    transform: scale(1.1);
   }
 </style>
