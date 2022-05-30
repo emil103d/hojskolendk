9c830f5e79f2952c069a3a8d0b0441c347543a50
emilie carlsen <emil103d@stud.kea.dk>
2022-05-30 19:17:49 +0200

    ...

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -3,8 +3,8 @@
   export let image;
 </script>
 
-<div class=" mx-auto my-8 relative z-0">
-  <img src={image} alt={title} />
+<div class=" mx-auto my-8 relative z-0 overflow-hidden">
+  <img class="hover:scale-105" src={image} alt={title} />
   <h3 class="bg-white pl-3 pt-3 pb-3 pr-3 z-3 absolute top-4 text-xs">
     {title}
   </h3>
@@ -20,9 +20,6 @@
   img {
     height: 24rem;
     width: 15rem;
-  }
-
-  div:hover {
-    transform: scale(1.1);
+    transition: all 0.2s ease-in-out;
   }
 </style>
