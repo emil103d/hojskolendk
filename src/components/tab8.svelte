aceae3f19af478876ff945ad1b5a58cc3d587f89
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-05-30 13:25:31 +0200

    Adventure - mangler kun carouselle

--- src/components/tab8.svelte
+++ src/components/tab8.svelte
@@ -1,1 +1,45 @@
-<h2>Adventure</h2>
+<main class="col-start-2 background_image   ">
+    <!-- SECTION 1 -->
+    <section>
+        <h2>ADVENTURE, OPLEVELSER & AKTIVITETER</h2>
+        <p>
+        Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på Rumrunners
+        eller snorkler efter havskildpadder. Derudover har vi haft en masse fede
+        ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck Island og
+        turtlewatch på programmet.
+        </p>
+
+        <hr>
+    </section>
+        
+    <section>
+        <p>Aktivitere på</p>
+        <h2>SRI LANKA</h2>
+        <div>
+            CAROUSELLE
+        </div>
+    </section>
+
+    <section>
+        <p>Aktivitere på</p>
+        <h2>SRI LANKA</h2>
+        <div>
+            CAROUSELLE
+        </div>
+    </section>
+
+    <section>
+        <p>Aktivitere på</p>
+        <h2>SRI LANKA</h2>
+        <div>
+            CAROUSELLE
+        </div>
+    </section>
+
+</main>
+
+<style>
+    .background_image {
+        background-image: url(src/_images/background_image_adventure.png);
+    }
+</style>
