cd53ed48939ee50e31ead0959bed6ca6c58c1631
Soph0298 <Soph0298@stud.kea.dk>
2022-05-30 18:23:07 +0200

    push

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,31 +37,18 @@
   <div>
     <img
       class=""
-      src="../src/_images/webp/forside.webp"
+      src="../src/_images/webp/dykke.webp"
       alt="Unge glade mennesker der er med på et højskoleophold"
     />
   </div>
 </section>
 <!-- section #1 slut -->
 
-<!-- section #2 Start -->
-
 <section id="section_2">
-  <div class="maxwidthwrappper m-5 justify-center">
+  <div class="maxwidthwrapper">
     <p class="mt-10 preh2">MANGE SPÆNDENDE DESTINATIONER</p>
     <h2 class="">SE VORES EVENTYRLIGE HØJSKOLEOPHOLD</h2>
     <div class="flex justify-center gap-5 hover:overflow-x-scroll">
-      <div class="w-14 h-14 bg-white rounded-full hover:scale-110">
-        <svg
-          width="18"
-          height="27"
-          viewBox="0 0 18 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M16.5 26L2 13L16.5 1" stroke="black" stroke-width="2" />
-        </svg>
-      </div>
       <Card
         image="../src/_images/webp/ophold1.webp"
         title="Maldiverne, Sri Lanka, Bali & Gili"
@@ -79,8 +66,8 @@
         title="Costa Rica, Mexico & Guatemala"
       />
     </div>
+    <button class="primarybutton grid m-auto">UDFORSK ALLE VORES OPHOLD</button>
   </div>
-  <button class="primarybutton m-auto">UDFORSK ALLE VORES OPHOLD</button>
 </section>
 
 <!-- section #2 Start -->
@@ -140,6 +127,8 @@ Jeg oplevede et fællesskab, jeg ikke sidenhen har prøvet, og jeg fik venskaber
 </section>
 
 <!-- Statistik section  slut -->
+
+<!-- section #2 Start -->
 <style>
   section {
     overflow: hidden;
@@ -171,6 +160,6 @@ Jeg oplevede et fællesskab, jeg ikke sidenhen har prøvet, og jeg fik venskaber
   }
 
   article {
-    width: 700px;
+    max-width: 700px;
   }
 </style>
