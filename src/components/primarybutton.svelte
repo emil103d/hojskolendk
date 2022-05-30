290e44461696d18fc76524ff098eab3f18c04f59
Soph0298 <Soph0298@stud.kea.dk>
2022-05-30 19:11:50 +0200

    primarybutton

--- src/components/primarybutton.svelte
+++ src/components/primarybutton.svelte
@@ -3,21 +3,47 @@
 <button class="primarybutton"> <slot /> </button>
 
 <style>
-  button,
   .primarybutton {
-    background-color: var(--orange);
-    font-size: var(--font_knap_m);
-    line-height: var(--line-l);
-    padding: var(--spacing-button);
-    font-size: var(--font_knap_m);
-    font-weight: 400;
-    color: var(--hvid);
-    border: 1px solid var(----primary-button-cta-background-color);
+    border: 1px solid transparent;
+    -webkit-transition: all 0.4s cubic-bezier(0.5, 0.24, 0, 1);
+    transition: all 0.4s cubic-bezier(0.5, 0.24, 0, 1);
+  }
+  .primarybutton::before {
+    content: "";
+    position: absolute;
+    left: 0px;
+    bottom: 0px;
+    z-index: -1;
+    width: 0%;
+    height: 1px;
+    background: #6098ff;
+    box-shadow: inset 0px 0px 0px #6098ff;
+    display: block;
+    -webkit-transition: all 0.4s cubic-bezier(0.5, 0.24, 0, 1);
+    transition: all 0.4s cubic-bezier(0.5, 0.24, 0, 1);
   }
 
-  button:hover,
+  .primarybutton:hover::before {
+    width: 100%;
+  }
+
+  .primarybutton::after {
+    content: "";
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    z-index: -1;
+    width: 0%;
+    height: 1px;
+    background: #6098ff;
+    -webkit-transition: all 0.4s cubic-bezier(0.5, 0.24, 0, 1);
+    transition: all 0.4s cubic-bezier(0.5, 0.24, 0, 1);
+  }
+  .primarybutton:hover::after {
+    width: 100%;
+  }
   .primarybutton:hover {
-    border: var(--button-cta-background-color-hover);
-    background-color: var(--button-cta-background-color-hover);
+    border-left: 1px solid #6098ff;
+    border-right: 1px solid #6098ff;
   }
 </style>
