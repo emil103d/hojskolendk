7e23424252a08c92e9732263a0020daf7ea20b1b
Soph0298 <Soph0298@stud.kea.dk>
2022-05-31 20:41:06 +0200

    knapper opdate

--- src/components/ophold_afrejse.svelte
+++ src/components/ophold_afrejse.svelte
@@ -2,58 +2,51 @@
   export let varig;
   export let pris;
   export let dato;
-  import PrimaryButton from "./primarybutton.svelte";
-  import SecondaryButton from "./secondarybutton.svelte";
+  import MyButton from "./button.svelte";
 </script>
 
+<section
+  class="background_color_boks p-4 md:m-0 lg:flex lg:justify-around lg:p-4 lg:items-center md:p-0"
+>
+  <div class="hidden lg:grid lg:text-center lg:justify-items-center">
+    <img class="w-12 h-12 md:block" src="../src/_icons/calender.png" alt="" />
+    <h3>Afrejse</h3>
+    <p class="tabeltxt">{dato}</p>
+  </div>
 
-<section class="background_color_boks p-4 md:m-0 lg:flex lg:justify-around lg:p-4 lg:items-center md:p-0">
-  
-  
-    <div class="hidden lg:grid lg:text-center lg:justify-items-center">
-      <img class="w-12 h-12 md:block" src="../src/_icons/calender.png" alt="" />
-      <h3>Afrejse</h3>
-      <p class="tabeltxt">{dato}</p>
-    </div>
+  <div class="grid justify-center py-4 lg:p-0">
+    <h3>Sri Lanka, Maldiverne & Thailand</h3>
 
-    
-
-    <div class="grid justify-center py-4 lg:p-0">
-      <h3>Sri Lanka, Maldiverne & Thailand</h3>
-      
-      <div class="flex gap-8 lg:gap-12 pt-4 justify-center lg:justify-start">
-        
-        <div class="flex items-center gap-2">
-          <img class="w-8 h-8" src="../src/_icons/calender.png" alt="" />
-          <div>
-            <p class="font-bold">Varighed </p>
-            <p class="tabeltxt">{varig}</p>
-          </div>
+    <div class="flex gap-8 lg:gap-12 pt-4 justify-center lg:justify-start">
+      <div class="flex items-center gap-2">
+        <img class="w-8 h-8" src="../src/_icons/calender.png" alt="" />
+        <div>
+          <p class="font-bold">Varighed</p>
+          <p class="tabeltxt">{varig}</p>
         </div>
+      </div>
 
-        <div class="hidden">
-          <img class="w-8 h-8 " src="../src/_icons/calender.png" alt="" />
-          <p class="font-bold">Afrejse </p>
-          <p class="tabeltxt">{dato}</p>
+      <div class="hidden">
+        <img class="w-8 h-8 " src="../src/_icons/calender.png" alt="" />
+        <p class="font-bold">Afrejse</p>
+        <p class="tabeltxt">{dato}</p>
+      </div>
+
+      <div class="h-full w-0.5 bg-black" />
+
+      <div class="flex items-center gap-2">
+        <img class="w-8 h-8" src="../src/_icons/calender.png" alt="" />
+        <div>
+          <p class="font-bold">Pris</p>
+          <p class="tabeltxt">{pris}</p>
         </div>
-
-        <div class="h-full w-0.5 bg-black" />
-
-        <div class="flex items-center gap-2">
-          <img class="w-8 h-8" src="../src/_icons/calender.png" alt="" />
-          <div>
-            <p class="font-bold">Pris </p>
-            <p class="tabeltxt">{pris}</p>
-          </div>
-        </div>
-      
       </div>
     </div>
-    <div class="flex lg:grid lg:grid-rows-2 justify-center py-4 gap-8 lg:gap-4">
-      <PrimaryButton>TILMELD</PrimaryButton>
-      <SecondaryButton>RESERVER</SecondaryButton>
-    </div>
-
+  </div>
+  <div class="flex lg:grid lg:grid-rows-2 justify-center py-4 gap-8 lg:gap-4">
+    <MyButton type="primary">TILMELD</MyButton>
+    <MyButton type="secondary">RESERVER</MyButton>
+  </div>
 </section>
 
 <style>
@@ -61,7 +54,3 @@
     background-color: var(--sandstone);
   }
 </style>
-
-
-
-
