a914ab4e0a7c7d656acfdb2ed23c3b67642b7bfe
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-06-01 14:23:04 +0200

    tab8 oprettet - mangler billeder

--- src/components/tab8.svelte
+++ src/components/tab8.svelte
@@ -2,7 +2,7 @@
   import Carouselle_aktiv from "../components/carouselle_aktiv.svelte";
 </script>
 
-<main class="col-start-2">
+<main class="col-start-2 w-[99%]">
   <!-- SECTION 1 -->
   <section class="maxwidthwrapper" id="no-pad">
     <h2>ADVENTURE, OPLEVELSER & AKTIVITETER</h2>
@@ -16,35 +16,29 @@
     <hr class="py-5">
   </section>
 
-  <p class="preh2">TILFØJELSER & AKTIVITETER</p>
-  <h3 class="">Tilføj endnu mere god stemning</h3>
-
   <!-- SECTION 2 -->
-  <section class="">
+  <section class="max-w-[1100px] px-4">
     <div class="">
-      <p class="preh2 text-center">AKTIVITET I OMRÅDET</p>
-      <h3 class="display text-center">SRI LANKA</h3>
-      <p class="text-center">Uendelige oplevelser og muligheder</p>
+      <p class="preh2">ADVENTURE & AKTIVITETER</p>
+      <h3 class="display">MALDIVERNE</h3>
       <Carouselle_aktiv />
     </div>
   </section>
 
   <!-- SECTION 3 -->
-  <section class="">
+  <section class="max-w-[1100px] px-4">
     <div class="">
-      <p class="preh2 text-center">SKRI LANKA</p>
-      <h3 class="display text-center">FRIUGE 2</h3>
-      <p class="text-center">Adventure & aktion</p>
+      <p class="preh2">ADVENTURE & AKTIVITETER</p>
+      <h3 class="display">SRI LANKA</h3>
       <Carouselle_aktiv />
     </div>
   </section>
 
   <!-- SECTION 4 -->
-  <section class="">
+  <section class="max-w-[1100px] px-4">
     <div class="">
-      <p class="preh2 text-center">HAWAII</p>
-      <h3 class="display text-center">FRIUGE 3</h3>
-      <p class="text-center">Afslapning, shopping og surfing</p>
+      <p class="preh2">ADVENTURE & AKTIVITETER</p>
+      <h3 class="display">THAILAND</h3>
       <Carouselle_aktiv />
     </div>
   </section>
@@ -56,10 +50,14 @@
     font-size: var(--font_h2);
     color: var(--sort);
     line-height: var(--line-m);
-    padding: var(--spacing-10);
   }
 
   #no-pad {
     padding-top: 0;
+  }
+
+  section {
+    padding-top: var(--spacing-80);
+    padding-bottom: var(--spacing-80);
   }
 </style>
