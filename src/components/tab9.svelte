08dcdd3e396b6848fb94e0b079f789793e1e1221
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-06-01 16:07:28 +0200

    ogu

--- src/components/tab9.svelte
+++ src/components/tab9.svelte
@@ -32,33 +32,7 @@
             alt=""
           />
         </div>
-        <div
-          class="lg:col-start-2 lg:col-span-2 lg:p-10 p-6  max-w-[50ch] m-auto"
-        >
-          <h3 class="pb-4">Lotte Schmidt</h3>
-          <p>
-            Jeg har, siden jeg var ganske lille, haft stor lyst og trang til at
-            udforske verden. Jeg har altid været interesseret i naturen; gåture
-            i skove og være ved vandet med forestillinger om at der lå verdener
-            uden for min forstand. Der er altid mere eventyr i horisonten, når
-            man ser på verden gennem mine briller. Jeg drømmer om at komme alle
-            de steder hen i verden, jeg endnu ikke har været (og der er
-            heldigvis mange), og tror på, at respekt og kærlighed for andre
-            mennesker, opstår i mødet og kendskabet med deres kultur og
-            verdensforståelse. Jeg har rejst siden jeg var 15 år gammel, på både
-            små-ture til Sverige i et par uger ad gangen og som 17-årig begav
-            jeg mig alene ud i Europa, hvor jeg tomlede den til Sydspanien, og
-            bagefter fløj til Island og drog hele vejen rundt på den smukke, rå
-            og vilde vulkanø. Det var første gang jeg for alvor kom i kontakt
-            med det, der fylder mig mest med glæde som menneske; at rejse og
-            møde mennesker på tværs af landegrænser.
-
-            <br />
-            <br />
-            Høj femmer <br />
-            Lotte
-          </p>
-        </div>
+        
       </div>
     </div>
   </section>
