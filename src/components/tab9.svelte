efedcbc200b73ed1f9d84f2a58619ae21903e217
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-06-01 15:57:37 +0200

    ok

--- src/components/tab9.svelte
+++ src/components/tab9.svelte
@@ -7,8 +7,8 @@
             <div class="lg:col-start-1">
                 <img class="w-full h-full object-cover" src="src/_images/rejsebrev_1.png" alt="">
             </div>
-        <div class="lg:col-start-2 lg:col-span-2 lg:p-10 max-w-[50ch]">
-        <h3 class="pb-4">Anton Skou Larsen</h3>
+        <div class="lg:col-start-2 lg:col-span-2 lg:p-10 p-6 max-w-[50ch] m-auto">
+        <h3 class="pb-4">Line Lund</h3>
         <p>Jeg har, siden jeg var ganske lille, haft stor lyst og trang til at udforske verden. Jeg har altid været interesseret i naturen; gåture i skove og være ved vandet med forestillinger om at der lå verdener uden for min forstand.
             Der er altid mere eventyr i horisonten, når man ser på verden gennem mine briller. Jeg drømmer om at komme alle de steder hen i verden, jeg endnu ikke har været (og der er heldigvis mange), og tror på, at respekt og kærlighed for andre mennesker, opstår i mødet og kendskabet med deres kultur og verdensforståelse.
             
@@ -16,22 +16,22 @@
             
             <br>
             <br>
-            Højskolesmil, <br>
-            Anton
+            God rejselyst, <br>
+            Line
         </p>
     </div>
     </div>  
     </div>
  </section>
 
- <section >
+ <section class="maxwidthwrapper">
    <div class="baggrundsfarve">
        <div class="xl:grid custom_grid">
            <div class="lg:col-start-1">
                <img class="w-full h-full object-cover" src="src/_images/rejsebrev_2.png" alt="">
            </div>
-       <div class="lg:col-start-2 lg:col-span-2 lg:p-10 max-w-[50ch]">
-       <h3 class="pb-4">Anton Skou Larsen</h3>
+       <div class="lg:col-start-2 lg:col-span-2 lg:p-10 p-6  max-w-[50ch] m-auto">
+       <h3 class="pb-4">Lotte Schmidt</h3>
        <p>Jeg har, siden jeg var ganske lille, haft stor lyst og trang til at udforske verden. Jeg har altid været interesseret i naturen; gåture i skove og være ved vandet med forestillinger om at der lå verdener uden for min forstand.
            Der er altid mere eventyr i horisonten, når man ser på verden gennem mine briller. Jeg drømmer om at komme alle de steder hen i verden, jeg endnu ikke har været (og der er heldigvis mange), og tror på, at respekt og kærlighed for andre mennesker, opstår i mødet og kendskabet med deres kultur og verdensforståelse.
            
@@ -39,21 +39,21 @@
            
            <br>
            <br>
-           Højskolesmil, <br>
-           Anton
+           Høj femmer <br>
+           Lotte
        </p>
    </div>
    </div>  
    </div>
 </section>
 
-<section >
+<section class="maxwidthwrapper">
    <div class="baggrundsfarve">
        <div class="xl:grid custom_grid">
            <div class="lg:col-start-1">
                <img class="w-full h-full object-cover" src="src/_images/webp/anton.webp" alt="">
            </div>
-       <div class="lg:col-start-2 lg:col-span-2 lg:p-10 max-w-[50ch]">
+       <div class="lg:col-start-2 lg:col-span-2 lg:p-10 p-6  max-w-[50ch] m-auto">
        <h3 class="pb-4">Anton Skou Larsen</h3>
        <p>Jeg har, siden jeg var ganske lille, haft stor lyst og trang til at udforske verden. Jeg har altid været interesseret i naturen; gåture i skove og være ved vandet med forestillinger om at der lå verdener uden for min forstand.
         Der er altid mere eventyr i horisonten, når man ser på verden gennem mine briller. Jeg drømmer om at komme alle de steder hen i verden, jeg endnu ikke har været (og der er heldigvis mange), og tror på, at respekt og kærlighed for andre mennesker, opstår i mødet og kendskabet med deres kultur og verdensforståelse.
@@ -62,7 +62,7 @@
         
            <br>
            <br>
-           Højskolesmil,  <br>
+           Højskolesmil, <br>
            Anton
        </p>
    </div>
