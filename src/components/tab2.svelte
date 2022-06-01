f5535d328caa6fddac26e46c6d71180708e5004c
Soph0298 <Soph0298@stud.kea.dk>
2022-06-01 11:28:07 +0200

    inkluderet

--- src/components/tab2.svelte
+++ src/components/tab2.svelte
@@ -1,110 +1,150 @@
-<main class="col-start-2" >
-    <!-- SECTION 1 -->
-    <section class="maxwidthwrapper" id="no-pad">
-        <h2>DET FÅR DU MED PÅ REJSEN</h2>
-        <p class="">
-        Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på Rumrunners
-        eller snorkler efter havskildpadder. Derudover har vi haft en masse fede
-        ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck Island og
-        turtlewatch på programmet.
-        </p>
-        <br>
+<main class="col-start-2">
+  <!-- SECTION 1 -->
+  <section class="maxwidthwrapper" id="no-pad">
+    <h2>DET FÅR DU MED PÅ REJSEN</h2>
+    <p class="">
+      Når vi ikke arbejder, nyder vi stranden, drikker lækre drinks på
+      Rumrunners eller snorkler efter havskildpadder. Derudover har vi haft en
+      masse fede ting som fx Jeepsafari i den dybe regnskov, snorkling ved Buck
+      Island og turtlewatch på programmet.
+    </p>
+    <br />
 
-        <hr class="py-5">
+    <hr class="py-5" />
 
-        <div class="grid gap-20 lg:col-start-1 ">
-            <div>
-                <p>DET FÅR DU MED</p>
-                <h3>INKLUDERET</h3>
-                <div class="grid inner_spacing">
-                    <div>
-                        <p><b>OVERORDNET</b></p>
-                        <ul>
-                            <li>Fly tur/retur: DK > New York > Caribien > Mexico > Hawaii > DK</li>
-                            <li>Indkvartering i New York, Caribien, Mexico og Hawaii </li>
-                            <li>Undervisning i ‘Projektstyring & Teambuilding’</li>   
-                            <li>Undervisning i ‘Frivilligt Arbejde & Kulturforståelse’</li>
-                            <li>Undervisning i ‘Selvudvikling & Kommunikation’</li>
-                            <li>Transfers fra lufthavne tur/retur</li>
-                            <li>15% rabat på vaccinationer hos ‘Danske Lægers Vaccinations Service’</li>
-                        </ul>
-                    </div>
-        
-                    <div>
-                        <p><b>ADVENTURE, AKTIVITETER & OPLEVELSER</b></p>
-                        <ul>
-                            <li>Fly tur/retur: DK > New York > Caribien > Mexico > Hawaii > DK</li>
-                            <li>Indkvartering i New York, Caribien, Mexico og Hawaii </li>
-                            <li>Undervisning i ‘Projektstyring & Teambuilding’</li>   
-                            <li>Undervisning i ‘Frivilligt Arbejde & Kulturforståelse’</li>
-                            <li>Undervisning i ‘Selvudvikling & Kommunikation’</li>
-                            <li>Transfers fra lufthavne tur/retur</li>
-                            <li>15% rabat på vaccinationer hos ‘Danske Lægers Vaccinations Service’</li>
-                        </ul>
-                    </div>
-        
-                    <div>
-                        <p><b>PRAKTISK</b></p>
-                        <ul>
-                            <li>Infobreve o  opholdet inden afrejse (med bl.a. pakkeliste, rejsetips, praktisk, etc.)</li>
-                            <li>Afrejsemøde ca. 5 uger inden afrejse (du møder dine rejsevenner og rejseleder) </li>
-                            <li>Dansk rejseleder på hele opholdet</li>   
-                            <li>Undervisning af engagerede, dygtige og danske undervisere</li>
-                            <li>Højskolebevis (kan bruges til kvote 2)</li>
-                        </ul>
-                    </div>
-                </div>   
-            </div>
-    
- 
-            <div class="lg:col-start-1">
-                <p>DET ER DER</p>
-                <h3>IKKE INKLUDERET</h3>
-                <div class="grid inner_spacing">
-                    <div>
-                        <p><b>OVERORDNET</b></p>
-                        <ul>
-                            <li>Lommepenge</li>
-                            <li>Indkvartering i New York, Caribien, Mexico og Hawaii </li>
-                            <li>Mad & Drikke</li>   
-                            <li>Rejseforsikring (+ evt. afbestillingsforsikring)</li>
-                            <li>Visum</li>
-                            <li>Vaccinationer</li>
-                            <li>Friuger</li>
-                        </ul>
-                    </div>
-
-                    <div>
-                        <p><b>SAMLET ESTIMAT</b></p>
-                        <ul>
-                            <li>Tidligere elever har samlet brugt ca. 30.000 DKK til 40.000 DKK undervejs på opholdet.</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
+    <div class="grid gap-20 lg:col-start-1 inkl">
+      <div>
+        <div class="inkluderet">
+          <img src="src/_icons/swag.png" alt="" />
+          <div>
+            <h3>INKLUDERET</h3>
+            <p class="preh2">DET KLARER VI</p>
+          </div>
         </div>
-    </section>
- 
-  
- </main>
- 
- 
+        <div class="grid inner_spacing mt-12">
+          <div>
+            <p>OVERORDNET</p>
+            <ul>
+              <li>
+                Fly tur/retur: DK > New York > Caribien > Mexico > Hawaii > DK
+              </li>
+              <li>Indkvartering i New York, Caribien, Mexico og Hawaii</li>
+              <li>Undervisning i ‘Projektstyring & Teambuilding’</li>
+              <li>Undervisning i ‘Frivilligt Arbejde & Kulturforståelse’</li>
+              <li>Undervisning i ‘Selvudvikling & Kommunikation’</li>
+              <li>Transfers fra lufthavne tur/retur</li>
+              <li>
+                15% rabat på vaccinationer hos ‘Danske Lægers Vaccinations
+                Service’
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p>ADVENTURE, AKTIVITETER & OPLEVELSER</p>
+            <ul>
+              <li>
+                Fly tur/retur: DK > New York > Caribien > Mexico > Hawaii > DK
+              </li>
+              <li>Indkvartering i New York, Caribien, Mexico og Hawaii</li>
+              <li>Undervisning i ‘Projektstyring & Teambuilding’</li>
+              <li>Undervisning i ‘Frivilligt Arbejde & Kulturforståelse’</li>
+              <li>Undervisning i ‘Selvudvikling & Kommunikation’</li>
+              <li>Transfers fra lufthavne tur/retur</li>
+              <li>
+                15% rabat på vaccinationer hos ‘Danske Lægers Vaccinations
+                Service’
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p>PRAKTISK</p>
+            <ul>
+              <li>
+                Infobreve o opholdet inden afrejse (med bl.a. pakkeliste,
+                rejsetips, praktisk, etc.)
+              </li>
+              <li>
+                Afrejsemøde ca. 5 uger inden afrejse (du møder dine rejsevenner
+                og rejseleder)
+              </li>
+              <li>Dansk rejseleder på hele opholdet</li>
+              <li>Undervisning af engagerede, dygtige og danske undervisere</li>
+              <li>Højskolebevis (kan bruges til kvote 2)</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="lg:col-start-1">
+        <div class="inkluderet">
+          <img src="src/_icons/swag.png" alt="" class="-rotate-90" />
+          <div>
+            <h3>IKKE INKLUDERET</h3>
+            <p class="preh2">DEN KLARER DU</p>
+          </div>
+        </div>
+        <div class="grid inner_spacing mt-10">
+          <div>
+            <p>OVERORDNET</p>
+            <ul>
+              <li>Lommepenge</li>
+              <li>Indkvartering i New York, Caribien, Mexico og Hawaii</li>
+              <li>Mad & Drikke</li>
+              <li>Rejseforsikring (+ evt. afbestillingsforsikring)</li>
+              <li>Visum</li>
+              <li>Vaccinationer</li>
+              <li>Friuger</li>
+            </ul>
+          </div>
+
+          <div>
+            <p>SAMLET ESTIMAT</p>
+            <ul>
+              <li>
+                Tidligere elever har samlet brugt ca. 30.000 DKK til 40.000 DKK
+                undervejs på opholdet.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
 <style>
-    section {
-        padding-top: var(--spacing-80);
-        padding-bottom: var(--spacing-80)
-    }
+  section {
+    padding-top: var(--spacing-80);
+    padding-bottom: var(--spacing-80);
+  }
 
-    li {
-        list-style:inside;
-        line-height: 1.5rem;
-    }
-    #no-pad {
+  .inkl p {
+    color: var(--txtcolor-700);
+  }
+
+  li {
+    list-style: inside;
+    line-height: var(--line-xl);
+    font-family: "lato", sans-serif;
+    color: var(--txtcolor-600);
+  }
+  #no-pad {
     padding-top: 0;
-    }
+  }
 
-    .inner_spacing {
-        gap: var(--spacing-20);
-    }
+  .inner_spacing {
+    gap: var(--spacing-20);
+  }
+
+  img {
+    width: 70px;
+  }
+
+  .inkluderet {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 20px;
+  }
 </style>
