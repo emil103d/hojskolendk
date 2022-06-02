5465c7073167996dc39cc97af49a847cacbfcd3f
Soph0298 <Soph0298@stud.kea.dk>
2022-06-02 23:18:58 +0200

    next button

--- src/components/tab3.svelte
+++ src/components/tab3.svelte
@@ -1,5 +1,4 @@
 <script>
-  import MyButton from "../components/button.svelte";
 </script>
 
 <main class="col-start-2">
@@ -189,23 +188,7 @@
     </div>
   </section>
 
-  <section class="flex justify-end px-4">
-    <MyButton type="linkbutton">
-      UNDERVISNINGEN <svg
-        class="m-auto"
-        width="18"
-        height="16"
-        viewBox="0 0 18 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M17.7071 8.70711C18.0976 8.31658 18.0976 7.68342 17.7071 7.29289L11.3431 0.928932C10.9526 0.538408 10.3195 0.538408 9.92893 0.928932C9.53841 1.31946 9.53841 1.95262 9.92893 2.34315L15.5858 8L9.92893 13.6569C9.53841 14.0474 9.53841 14.6805 9.92893 15.0711C10.3195 15.4616 10.9526 15.4616 11.3431 15.0711L17.7071 8.70711ZM17 7L-4.76837e-07 7V9L17 9V7Z"
-          fill="#929292"
-        />
-      </svg>
-    </MyButton>
-  </section>
+  <section class="flex justify-end px-4" />
 </main>
 
 <style>
