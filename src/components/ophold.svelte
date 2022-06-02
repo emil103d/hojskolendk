561b463728210e4279213b39051303090fc12b5b
Soph0298 <Soph0298@stud.kea.dk>
2022-06-02 23:15:47 +0200

    link til singlepage

--- src/components/ophold.svelte
+++ src/components/ophold.svelte
@@ -16,55 +16,57 @@
   class="ophold h-auto w-full block md:flex max-w-[1400px] items-center mb-5 pb-6"
   transition:scale={{ duration: 1000, easing: expoInOut }}
 >
-  <div class="h-full w-auto object-cover min-w-[200px]">
-    <img src={image} alt={title} class="h-full w-auto object-cover" />
-  </div>
-  <div>
-    <div class="m-5">
-      <h3>{title}</h3>
-      <p>{subtitle}</p>
+  <a href="/singlepage">
+    <div class="h-full w-auto object-cover min-w-[200px]">
+      <img src={image} alt={title} class="h-full w-auto object-cover" />
     </div>
-    <div class="grid grid-cols-2 grid-rows-2 md:flex mt-10 m-5 gap-6">
-      <div class="flex col-start-1 md: place-items-center">
-        <img class="w-8 h-8 mr-6" src="flyafgang.png" alt="flyafgangs ikon" />
-        <div class="">
-          <p class="">Næste afrejse:</p>
-          <p class="tabeltxt">{dato}</p>
-        </div>
-        <div class="h-10 hidden md:block bg-black w-0.5 ml-3 mr-6" />
+    <div>
+      <div class="m-5">
+        <h3>{title}</h3>
+        <p>{subtitle}</p>
       </div>
-
-      <div class="flex col-start-2 place-items-center ">
-        <img class="w-8 h-8 mr-5" src="calender.png" alt="ikon af kalender" />
-        <div>
-          <p class="">Varighed:</p>
-          <p class="tabeltxt">{varig}</p>
+      <div class="grid grid-cols-2 grid-rows-2 md:flex mt-10 m-5 gap-6">
+        <div class="flex col-start-1 md: place-items-center">
+          <img class="w-8 h-8 mr-6" src="flyafgang.png" alt="flyafgangs ikon" />
+          <div class="">
+            <p class="">Næste afrejse:</p>
+            <p class="tabeltxt">{dato}</p>
+          </div>
+          <div class="h-10 hidden md:block bg-black w-0.5 ml-3 mr-6" />
         </div>
-        <div class="h-10 bg-black w-0.5 ml-3 mr-6 hidden md:block" />
+
+        <div class="flex col-start-2 place-items-center ">
+          <img class="w-8 h-8 mr-5" src="calender.png" alt="ikon af kalender" />
+          <div>
+            <p class="">Varighed:</p>
+            <p class="tabeltxt">{varig}</p>
+          </div>
+          <div class="h-10 bg-black w-0.5 ml-3 mr-6 hidden md:block" />
+        </div>
+
+        <div class="flex col-start-1 row-start-2 place-items-center">
+          <img class="w-8 h-8 mr-5" src="price.png" alt="pris ikon" />
+          <div>
+            <p class="">Pris:</p>
+            <p class="tabeltxt">{pris}</p>
+          </div>
+          <div class="h-10 bg-black w-0.5 ml-3 mr-6 hidden md:block" />
+        </div>
+
+        <div class="flex col-start-2 row-start-2 place-items-center ">
+          <img class="w-8 h-8 mr-5" src="faelleskab.png" alt="fælleskab ikon" />
+          <div>
+            <p class="">Undervisning:</p>
+            <p class="tabeltxt">{undervisning}</p>
+          </div>
+          <div class="h-10 bg-black w-0.5 ml-3 mr-6 hidden md:block" />
+        </div>
       </div>
-
-      <div class="flex col-start-1 row-start-2 place-items-center">
-        <img class="w-8 h-8 mr-5" src="price.png" alt="pris ikon" />
-        <div>
-          <p class="">Pris:</p>
-          <p class="tabeltxt">{pris}</p>
-        </div>
-        <div class="h-10 bg-black w-0.5 ml-3 mr-6 hidden md:block" />
-      </div>
-
-      <div class="flex col-start-2 row-start-2 place-items-center ">
-        <img class="w-8 h-8 mr-5" src="faelleskab.png" alt="fælleskab ikon" />
-        <div>
-          <p class="">Undervisning:</p>
-          <p class="tabeltxt">{undervisning}</p>
-        </div>
-        <div class="h-10 bg-black w-0.5 ml-3 mr-6 hidden md:block" />
+      <div class="grid justify-center md:justify-start ml-3 pt-4">
+        <MyButton type="primary">Læs mere</MyButton>
       </div>
     </div>
-    <div class="grid justify-center md:justify-start ml-3 pt-4">
-      <MyButton type="primary">Læs mere</MyButton>
-    </div>
-  </div>
+  </a>
 </section>
 
 <style>
