b7e3278be62b77ec42c8f00d4d4be95937236fd8
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-06-03 02:52:11 +0200

    problemer med ribbon

--- src/components/ophold_afrejse.svelte
+++ src/components/ophold_afrejse.svelte
@@ -11,7 +11,7 @@
   <div class="hidden lg:grid lg:text-center lg:justify-items-center">
     <img
       class="w-12 h-12 md:block"
-      src=".calender.png"
+      src="calender.png"
       alt="ikon af kalender"
     />
     <h3>Afrejse</h3>
@@ -20,6 +20,7 @@
 
   <div class="grid justify-center py-4 lg:p-0">
     <h3>Sri Lanka, Maldiverne & Thailand</h3>
+  
 
     <div class="flex gap-8 lg:gap-12 pt-4 justify-center lg:justify-start">
       <div class="flex items-center gap-2">
@@ -39,7 +40,7 @@
       <div class="h-full w-0.5 bg-black" />
 
       <div class="flex items-center gap-2">
-        <img class="w-8 h-8" src="calender.png" alt="ikon af kalender" />
+        <img class="w-8 h-8" src="price.png" alt="ikon af kalender" />
         <div>
           <p class="font-bold">Pris</p>
           <p class="tabeltxt">{pris}</p>
